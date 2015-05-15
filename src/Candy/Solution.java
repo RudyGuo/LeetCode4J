826c78333650e304f52a072c2db6f4561ab2f8dd
RudyGuo <guorudi@foxmail.com>
2015-05-15 22:18:48 +0800

    add Minmum Size Subarrsy Sum

--- src/Candy/Solution.java
+++ src/Candy/Solution.java
@@ -1,7 +1,12 @@
 package Candy;
 
 public class Solution {
-    public int candy(int[] ratings) {
-        return -1;
-    }
+	public int candy(int[] ratings) {
+		return -1;
+	}
+
+	public static void main(String args[]) {
+
+		System.out.println("test");
+	}
 }
