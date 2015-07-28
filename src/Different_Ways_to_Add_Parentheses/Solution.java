e3c57f16c32737d4e0da28f20d6653e2c6641447
RudyGuo <guorudi@foxmail.com>
2015-07-28 22:28:46 +0800

    add problems

--- src/Different_Ways_to_Add_Parentheses/Solution.java
+++ src/Different_Ways_to_Add_Parentheses/Solution.java
@@ -4,7 +4,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+/*Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
 
+
+Example 1
+Input: "2-1-1".
+
+((2-1)-1) = 0
+(2-(1-1)) = 2
+Output: [0, 2]
+
+
+Example 2
+Input: "2*3-4*5"
+
+(2*(3-(4*5))) = -34
+((2*3)-(4*5)) = -14
+((2*(3-4))*5) = -10
+(2*((3-4)*5)) = -10
+(((2*3)-4)*5) = 10
+Output: [-34, -14, -10, -10, 10]
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.*/
 public class Solution {
 	List<Integer> numbers = new LinkedList<>();
 	List<Character> opraters = new ArrayList<>();
